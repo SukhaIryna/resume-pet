789d56f15ec1d4198e1e339480290573f3091a7c
SukhaIryna <suhayairina1@gmail.com>
2023-04-30 16:07:47 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -224,17 +224,44 @@ router.get('/work', function (req, res) {
     header,
 
     main: {
-      work: [
+      title: 'Work history',
+      works: [
         {
           position: 'Junior Fullstak Developer',
           company: {
             name: 'IT Brains',
-            url: 'https://it-brains.com.ua/',
+            url: null,
           },
           duration: {
             from: '10.10.2022',
-            to: '22.03.2023',
+            to: null,
           },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about:
+                'It is good to be able to write a resume',
+
+              title: 'Project stacks',
+
+              stacksAmount: 8,
+              stacks: [
+                { name: 'React.js' },
+
+                { name: 'HTML /CSS' },
+
+                { name: 'Node.js' },
+              ],
+              title: 'Project awards',
+              awardsAmount: 2,
+              awards: [
+                { name: 'Super' },
+                { name: 'Puper' },
+              ],
+            },
+          ],
         },
       ],
     },
@@ -244,5 +271,121 @@ router.get('/work', function (req, res) {
 })
 
 // ================================================================
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index', {})
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/person', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('person', {
+    person: {
+      name: 'Emma Johnson',
+      age: 32,
+      gender: 'Female',
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '10001',
+        country: 'USA',
+      },
+      education: [
+        {
+          degree: 'Bachelor of Science',
+          major: 'Computer Science',
+          university:
+            'Massachusetts Institute of Technology',
+          graduationYear: 2012,
+        },
+      ],
+      workExperience: [
+        {
+          company: 'Google',
+          title: 'Software Engineer',
+          startDate: '2012-06-01',
+          endDate: '2016-12-31',
+          responsibilities: [
+            'Developed new features for Google Maps',
+            'Worked on improving search algorithms',
+          ],
+          year_founded: 1990,
+          industry: 'Technology',
+          employees: [
+            {
+              name: 'John Smith',
+              position: 'CEO',
+              department: 'Executive',
+              projects: [
+                {
+                  name: 'Project Alpha',
+                  description:
+                    'Developing new software platform',
+                  status: 'In Progress',
+                  teams: [
+                    {
+                      team_name: 'Awesome Team',
+                      team_leader: {
+                        name: 'John Smith',
+                        title: 'Team Leader',
+                        email: 'john.smith@example.com',
+                      },
+                      team_members: [
+                        {
+                          name: 'Alice Johnson',
+                          title: 'Software Engineer',
+                          email:
+                            'alice.johnson@example.com',
+                          skills: ['Java', 'Python', 'SQL'],
+                          projects: [
+                            {
+                              name: 'Project A',
+                              description:
+                                'Lorem ipsum dolor sit amet',
+                              technologies: [
+                                'Java',
+                                'Spring Framework',
+                              ],
+                              team_members: [
+                                {
+                                  name: 'Bob Lee',
+                                  title:
+                                    'Software Engineer',
+                                },
+                                {
+                                  name: 'Cindy Chen',
+                                  title: 'UI Designer',
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  })
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
